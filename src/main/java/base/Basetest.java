e38546adb375afe9f9f9595c8735b726abf9f837
Anush Office <anush@spicegems.com>
2025-12-23 17:50:40 +0530

    working code

---
+++ src/main/java/base/Basetest.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+
+
+public class Basetest {
+WebDriver driver;
+	
+	
+@BeforeSuite
+public void openUrl() {
+	driver = new ChromeDriver();
+	driver.get("https://spicegems-064.myshopify.com/");
+	driver.manage().window().maximize();
+}
+public void tearDown() {
+	driver.quit();
+}
+
+}
